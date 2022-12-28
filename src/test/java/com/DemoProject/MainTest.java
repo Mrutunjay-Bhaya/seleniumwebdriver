6b9969f1053cb2c3148087ed3229d8e97096177b
mrutunjay <sib>
2022-12-28 17:15:23 +0530

    Add 2 more test steps

--- src/test/java/com/DemoProject/MainTest.java
+++ src/test/java/com/DemoProject/MainTest.java
@@ -1,5 +1,6 @@
 package com.DemoProject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -15,13 +16,22 @@ public class MainTest
 	WebDriver driver;
     /**
      * Rigorous Test :-)
+     * @throws InterruptedException 
      */
    @Test
-    public void setUp()
+    public void setUp() throws InterruptedException
     {
        WebDriverManager.chromedriver().setup();
        driver=new ChromeDriver();
-       driver.get("https://makemytrip.com");
+       driver.get("https://demoqa.com/");
        System.out.println("run sucsesful");
+       driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+       driver.findElement(By.xpath("//span[text()='Text Box']")).click();
+//       driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+//       driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+//       driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+//       
+       Thread.sleep(5000);
+       driver.quit();
     }
 }
